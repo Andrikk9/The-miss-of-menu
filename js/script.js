05aff3861597133fab74755a11d3cf7c6fb45801
Andrikk9 <146778920+Andrikk9@users.noreply.github.com>
2024-05-09 23:59:14 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.carousel').slick(
+        {
+            dots: true,
+            infinite: false,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        }
+    );
+});
